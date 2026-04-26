f19eb49df4439f2eb6b941d3b2dad4b8bf35378b
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2026-04-27 05:06:43 +0530

    chore(tooling): adopt @saas-maker/eslint-config + tsconfig

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,11 +1,3 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import config from "@saas-maker/eslint-config/next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-]);
-
-export default eslintConfig;
+export default config;
